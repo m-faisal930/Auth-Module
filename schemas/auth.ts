5745272165e5aa3c20c2fba9b097affc6ff2f628
m-faisal930 <faisal.mr333@gmail.com>
2025-09-29 16:15:16 +0500

    ADDED: user model schema

---
+++ schemas/auth.ts
@@ -0,0 +1,21 @@
+// schemas/auth.ts
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  email: z.string().email(),
+  username: z.string().min(1),
+  password: z.string().min(8)
+    .regex(/[A-Z]/, "Must have an uppercase letter")
+    .regex(/[0-9]/, "Must have a number"),
+});
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1),
+});
+
+export const resetPasswordSchema = z.object({
+  password: z.string().min(8)
+    .regex(/[A-Z]/, "Must have an uppercase letter")
+    .regex(/[0-9]/, "Must have a number"),
+});
