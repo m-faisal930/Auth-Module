640f9ef65c9bbb50e4c43dfbb02e2ceb330057c1
m-faisal930 <faisal.mr333@gmail.com>
2025-09-30 18:50:08 +0500

    UPDATED: toaster import

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,6 +4,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { toast, Bounce } from 'react-toastify';
 
 export default function Signup() {
   const [message, setMessage] = useState("");
