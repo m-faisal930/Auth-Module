9fc5df83f6cfab8e1204a38c9eebe1ef30cc4193
m-faisal930 <faisal.mr333@gmail.com>
2025-09-30 16:45:11 +0500

    UPDATED: signup ui

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,17 +2,17 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
   const [message, setMessage] = useState("");
+    const router = useRouter();
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
-    console.log(formData.get("username"));
-    console.log(formData.get("email"));
-    console.log(formData.get("password"));
 
     const res = await fetch("/api/auth/signup", {
       method: "POST",
@@ -26,17 +26,93 @@ export default function Signup() {
 
     const data = await res.json();
     setMessage(data.message);
+    router.push("/login");
   }
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-3">
-      <input name="username" type="text" placeholder="Username" className="border p-2" />
-      <input name="email" type="email" placeholder="Email" className="border p-2" />
-      <input name="password" type="password" placeholder="Password" className="border p-2" />
-      <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-        Signup
-      </button>
-      {message && <p>{message}</p>}
-    </form>
+<div className="bg-gray-50">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <Link
+      href="/"
+      className="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
+    >
+
+      Authontication App
+    </Link>
+    <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+          Sign up to your account
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" >
+                      <div>
+            <label
+              htmlFor="username"
+              className="block mb-2 text-sm font-medium text-gray-900 "
+            >
+                Your username
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 "
+              placeholder="username"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5"
+              placeholder="name@company.com"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 "
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 "
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full text-white bg-gray-600 hover:cursor-pointer hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Sign up
+          </button>
+          <p className="text-sm font-light text-gray-500">
+            Already have account?{" "}
+            <Link
+              href="/login"
+              className="font-medium text-gray-600 hover:underline "
+            >
+                Sign in
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
   );
 }
